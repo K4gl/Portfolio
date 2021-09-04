fffe22e673442880a7d423b39917957da4416660
K4gl <kishanthapa06@gmail.com>
2021-09-04 13:10:59 +1000

    portfolio

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+    $(".slider").slick({
+        arrows:false,
+        dots:true,
+        appendDots:".slider-dots",
+        dotsClass:"dots"
+    });
+    
+    });
+
+    
+    
+    
